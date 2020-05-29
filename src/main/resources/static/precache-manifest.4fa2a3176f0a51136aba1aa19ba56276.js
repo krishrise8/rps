399f1bf97d219e744ae5de5123e31b7149273391
Krish <krish@Krishs-MacBook-Pro.local>
2020-05-29 12:15:41 -0400

    Fixing js for project

---
+++ src/main/resources/static/precache-manifest.4fa2a3176f0a51136aba1aa19ba56276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afc06f74f5063dcaa7234df7b03f93e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b8613b0323caee1a4c1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69526407e90a8793dcaa",
+    "url": "/static/js/2.8c0ebc5b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8c0ebc5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8613b0323caee1a4c1",
+    "url": "/static/js/main.12e36fab.chunk.js"
+  },
+  {
+    "revision": "760175ebdf9348d310ae",
+    "url": "/static/js/runtime-main.a4a027bd.js"
+  }
+]);
